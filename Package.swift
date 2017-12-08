2165bed84c09e74d99de880d8aa1f5f77d77d0ac
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2017-12-08 12:51:35 +0100

    Fixes Swift PM issues

--- Package.swift
+++ Package.swift
@@ -7,9 +7,7 @@ let package = Package(
     name: "BartlebysCore",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "BartlebysCore",
-            targets: ["BartlebysCore"]),
+        .library( name: "BartlebysCore", targets: ["BartlebysCore"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +17,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "BartlebysCore",
-            dependencies: []),
+            name: "BartlebysCore", dependencies: [],path: "Sources"),
         .testTarget(
-            name: "BartlebysCoreTests",
-            dependencies: ["BartlebysCore"]),
+            name: "BartlebysCoreTests",dependencies: ["BartlebysCore"],path: "Tests"),
     ]
 )
---
+++ Sources/BartlebyCore/Schemes.swift
@@ -0,0 +1,15 @@
+//
+//  Schemes.swift
+//  BartlebyCore
+//
+//  Created by Benoit Pereira da silva on 07/12/2017.
+//  Copyright © 2017 MusicWork. All rights reserved.
+//
+
+import Foundation
+
+public enum Scheme:String{
+
+    case https = "https://"
+
+}
