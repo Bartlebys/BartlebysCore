1d6f59f32f301efb131d5406f891670782b3e41c
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2017-12-09 08:09:22 +0100

    Added XCode projects that can be linked to simplify the dev. workflow

---
+++ Projects/BartlebysCoreiOS/BartlebysCoreiOS/BartlebysCoreiOS.h
@@ -0,0 +1,19 @@
+//
+//  BartlebysCoreiOS.h
+//  BartlebysCoreiOS
+//
+//  Created by Laurent Morvillier on 08/12/2017.
+//  Copyright © 2017 Bartleby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BartlebysCoreiOS.
+FOUNDATION_EXPORT double BartlebysCoreiOSVersionNumber;
+
+//! Project version string for BartlebysCoreiOS.
+FOUNDATION_EXPORT const unsigned char BartlebysCoreiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BartlebysCoreiOS/PublicHeader.h>
+
+
