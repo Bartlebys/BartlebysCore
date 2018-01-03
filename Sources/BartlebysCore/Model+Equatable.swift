43b9f3fb50579ff36b2cd4fb51963befbab7f0cc
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-01-03 17:22:04 +0100

    Added Global equalityOf function for Models

--- Sources/BartlebysCore/Model+Equatable.swift
+++ Sources/BartlebysCore/Model+Equatable.swift
@@ -10,7 +10,18 @@ import Foundation
 
 extension Model{
 
+    // We have encountered serious QUIRKS With `Equatable` + OBJC runtime (with swift 4)
+    // Such inconsistencies or bugs where very difficult to debug o
+    // So we have decided to create global functions equalityOf(...)
+    // The Equatable implementation relies on this function
+
     public static func ==(lhs: Model, rhs: Model) -> Bool {
-        return lhs.id == rhs.id
+        return equalityOf(lhs, rhs)
     }
 }
+
+public func equalityOf(_ lhs: Model?, _ rhs: Model?)-> Bool {
+    let equality = lhs?.UID == rhs?.UID
+    return equality
+}
+
