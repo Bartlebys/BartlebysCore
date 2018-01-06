4da3b3c09a4d2c4646449235ddbdf2604fd98e7d
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-01-06 12:27:39 +0100

    Alias encoding is using BartlebysCore.MODELS_PRIMARY_KEY

--- Sources/BartlebysCore/Alias.swift
+++ Sources/BartlebysCore/Alias.swift
@@ -46,9 +46,26 @@ public enum AliasResolverError:Error{
 
 /// The alias struct
 public struct Alias:Codable{
-    public let UID:String
+
+    // We use
+    public let UID:UID
+
+    public init(UID:UID) {
+        self.UID = UID
+    }
+
+    // MARK: - Codable
+
+    // We use the Model.ModelCodingKeys because we want to be able define if the id is encoded as `_id` or `id`
+
+    public init(from decoder: Decoder) throws{
+        let values = try decoder.container(keyedBy: Model.ModelCodingKeys.self)
+        self.UID = try values.decode(String.self,forKey:BartlebysCore.MODELS_PRIMARY_KEY)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy:  Model.ModelCodingKeys.self)
+        try container.encode(self.UID,forKey:BartlebysCore.MODELS_PRIMARY_KEY)
+    }
+
 }
-
-
-
-
