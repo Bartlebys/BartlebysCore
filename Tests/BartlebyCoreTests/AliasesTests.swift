7b48e5f13e7f9fa53afacfe02df4b515a2e4b0ba
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-01-08 12:47:26 +0100

    Added 3 Optional Aliases resolution tests

--- Tests/BartlebyCoreTests/AliasesTests.swift
+++ Tests/BartlebyCoreTests/AliasesTests.swift
@@ -16,9 +16,11 @@ class AliasesTests: BaseDataPointTestCase{
 
     static var allTests = [
         ("test001_genericAliasOfResolutionShouldFail", test001_genericAliasOfResolutionShouldFail),
-        ("test002_genericAliasOfResolution", test002_genericAliasOfResolution),
+        ("test002A_genericAliasOfResolution", test002A_genericAliasOfResolution),
+        ("test002B_optionalGenericAliasOfResolution", test002B_optionalGenericAliasOfResolution),
         ("test003A_genericAliasesOfResolution", test003A_genericAliasesOfResolution),
         ("test003B_genericAliasesOfResolution", test003B_genericAliasesOfResolution),
+        ("test003C_optionalGenericAliasesOfResolution",test003C_optionalGenericAliasesOfResolution),
         ("test004_AliasResolution", test004_AliasResolution),
         ("test005_AliasResolution", test005_AliasResolution),
         ("test006A_AliasesListResolution",test006A_AliasesListResolution),
@@ -49,7 +51,7 @@ class AliasesTests: BaseDataPointTestCase{
         }
     }
 
-    func test002_genericAliasOfResolution(){
+    func test002A_genericAliasOfResolution(){
 
         let datapoint = self.getNewDataPoint()
 
@@ -72,68 +74,111 @@ class AliasesTests: BaseDataPointTestCase{
 
     }
 
+    func test002B_optionalGenericAliasOfResolution(){
+
+        let datapoint = self.getNewDataPoint()
+
+        let kd = KeyedData()
+        kd.key = "theKey"
+        kd.data = "theValue".data(using: .utf8)!
+        datapoint.keyedDataCollection.append(kd)
+
+        let o = TestObject()
+        datapoint.testObjectsCollection.append(o)
+        o.aliasOfKD = kd.aliasOf()
+
+        if let kdFromAlias:KeyedData = o.optionalInstance(from: o.aliasOfKD!){
+            XCTAssert(kdFromAlias == kd, "kdFromAlias should be equal to kd")
+            XCTAssert(kdFromAlias === kd, "kdFromAlias should be kd")
+        }else{
+            XCTFail("kdFromAlias should exist")
+        }
+    }
 
 
     func test003A_genericAliasesOfResolution(){
-        do {
-            let datapoint = self.getNewDataPoint()
 
-            let metrics1 = Metrics()
-            metrics1.operationName = "op1"
-            datapoint.metricsCollection.append(metrics1)
+        let datapoint = self.getNewDataPoint()
 
-            let o = TestObject()
-            datapoint.testObjectsCollection.append(o)
+        let metrics1 = Metrics()
+        metrics1.operationName = "op1"
+        datapoint.metricsCollection.append(metrics1)
 
-            // Append an aliasOf metrics1
-            o.aliasesOfMetrics.append(metrics1.aliasOf())
+        let o = TestObject()
+        datapoint.testObjectsCollection.append(o)
 
-            //
-            do{
-                if let metricsFromAliasesOfMetrics:Metrics = try o.instances(from: o.aliasesOfMetrics).first{
-                    XCTAssert(metricsFromAliasesOfMetrics == metrics1, "kdFromAlias should be equal to kd")
-                    XCTAssert(metricsFromAliasesOfMetrics === metrics1, "kdFromAlias should be kd")
-                }else{
-                    XCTFail(" o.aliasesOfMetrics should contains at least one metrics")
-                }
-            }catch{
-                XCTFail("metricsFromAliasesOfMetrics resolution did fail")
+        // Append an aliasOf metrics1
+        o.aliasesOfMetrics.append(metrics1.aliasOf())
+
+        //
+        do{
+            if let metricsFromAliasesOfMetrics:Metrics = try o.instances(from: o.aliasesOfMetrics).first{
+                XCTAssert(metricsFromAliasesOfMetrics == metrics1, "kdFromAlias should be equal to kd")
+                XCTAssert(metricsFromAliasesOfMetrics === metrics1, "kdFromAlias should be kd")
+            }else{
+                XCTFail(" o.aliasesOfMetrics should contains at least one metrics")
             }
         }catch{
-            XCTFail("\(error)")
+            XCTFail("metricsFromAliasesOfMetrics resolution did fail")
         }
     }
 
     func test003B_genericAliasesOfResolution(){
-        do {
-            let datapoint = self.getNewDataPoint()
 
-            let metrics1 = Metrics()
-            metrics1.operationName = "op1"
-            datapoint.metricsCollection.append(metrics1)
+        let datapoint = self.getNewDataPoint()
 
-            let o = TestObject()
-            datapoint.testObjectsCollection.append(o)
+        let metrics1 = Metrics()
+        metrics1.operationName = "op1"
+        datapoint.metricsCollection.append(metrics1)
 
-            // Append 3 aliasOf metrics1
-            o.aliasesOfMetrics.append(metrics1.aliasOf())
-            o.aliasesOfMetrics.append(metrics1.aliasOf())
-            o.aliasesOfMetrics.append(metrics1.aliasOf())
-            do{
-                let metricsFromAliasesOfMetrics:[Metrics] = try o.instances(from: o.aliasesOfMetrics)
-                XCTAssert(metricsFromAliasesOfMetrics.count == 3, "metricsFromAliasesOfMetrics.count should be equal to 3, current count = \(metricsFromAliasesOfMetrics.count)")
-                for metrics in metricsFromAliasesOfMetrics{
-                    // Any metrics should refer to metrics1
-                    XCTAssert(metrics == metrics1, "metrics should be equal to metrics1")
-                    XCTAssert(metrics === metrics1, "metrics should be metrics1")
-                }
-            }catch{
-                XCTFail("metricsFromAliasesOfMetrics resolution did fail")
+        let o = TestObject()
+        datapoint.testObjectsCollection.append(o)
+
+        // Append 3 aliasOf metrics1
+        o.aliasesOfMetrics.append(metrics1.aliasOf())
+        o.aliasesOfMetrics.append(metrics1.aliasOf())
+        o.aliasesOfMetrics.append(metrics1.aliasOf())
+        do{
+            let metricsFromAliasesOfMetrics:[Metrics] = try o.instances(from: o.aliasesOfMetrics)
+            XCTAssert(metricsFromAliasesOfMetrics.count == 3, "metricsFromAliasesOfMetrics.count should be equal to 3, current count = \(metricsFromAliasesOfMetrics.count)")
+            for metrics in metricsFromAliasesOfMetrics{
+                // Any metrics should refer to metrics1
+                XCTAssert(metrics == metrics1, "metrics should be equal to metrics1")
+                XCTAssert(metrics === metrics1, "metrics should be metrics1")
             }
         }catch{
-            XCTFail("\(error)")
+            XCTFail("metricsFromAliasesOfMetrics resolution did fail")
         }
     }
+
+
+    func test003C_optionalGenericAliasesOfResolution(){
+
+        let datapoint = self.getNewDataPoint()
+
+        let metrics1 = Metrics()
+        metrics1.operationName = "op1"
+        datapoint.metricsCollection.append(metrics1)
+
+        let o = TestObject()
+        datapoint.testObjectsCollection.append(o)
+
+        // Append 3 aliasOf metrics1
+        o.aliasesOfMetrics.append(metrics1.aliasOf())
+        o.aliasesOfMetrics.append(metrics1.aliasOf())
+        o.aliasesOfMetrics.append(metrics1.aliasOf())
+
+        let metricsFromAliasesOfMetrics:[Metrics] = o.optionalInstances(from: o.aliasesOfMetrics)
+        XCTAssert(metricsFromAliasesOfMetrics.count == 3, "metricsFromAliasesOfMetrics.count should be equal to 3, current count = \(metricsFromAliasesOfMetrics.count)")
+        for metrics in metricsFromAliasesOfMetrics{
+            // Any metrics should refer to metrics1
+            XCTAssert(metrics == metrics1, "metrics should be equal to metrics1")
+            XCTAssert(metrics === metrics1, "metrics should be metrics1")
+        }
+
+
+    }
+
 
 
     // MARK: - Not generic == Alias
@@ -213,33 +258,31 @@ class AliasesTests: BaseDataPointTestCase{
 
 
     func test006B_AliasesListResolution(){
-        do {
-            let datapoint = self.getNewDataPoint()
 
-            let metrics1 = Metrics()
-            metrics1.operationName = "op1"
-            datapoint.metricsCollection.append(metrics1)
+        let datapoint = self.getNewDataPoint()
 
-            let o = TestObject()
-            datapoint.testObjectsCollection.append(o)
+        let metrics1 = Metrics()
+        metrics1.operationName = "op1"
+        datapoint.metricsCollection.append(metrics1)
 
-            // Append 3 metrics1's alias
-            o.aliasesList.append(metrics1.alias())
-            o.aliasesList.append(metrics1.alias())
-            o.aliasesList.append(metrics1.alias())
-            do{
-                let metricsFromMetricsAliasesList:[Metrics] = try o.instances(from: o.aliasesList)
-                XCTAssert(metricsFromMetricsAliasesList.count == 3, "metricsFromMetricsAliasesList.count should be equal to 3, current count = \(metricsFromMetricsAliasesList.count)")
-                for metrics in metricsFromMetricsAliasesList{
-                    // Any metrics should refer to metrics1
-                    XCTAssert(metrics == metrics1, "metrics should be equal to metrics1")
-                    XCTAssert(metrics === metrics1, "metrics should be metrics1")
-                }
-            }catch{
-                XCTFail("metricsFromAliasesOfMetrics resolution did fail")
+        let o = TestObject()
+        datapoint.testObjectsCollection.append(o)
+
+        // Append 3 metrics1's alias
+        o.aliasesList.append(metrics1.alias())
+        o.aliasesList.append(metrics1.alias())
+        o.aliasesList.append(metrics1.alias())
+        do{
+            let metricsFromMetricsAliasesList:[Metrics] = try o.instances(from: o.aliasesList)
+            XCTAssert(metricsFromMetricsAliasesList.count == 3, "metricsFromMetricsAliasesList.count should be equal to 3, current count = \(metricsFromMetricsAliasesList.count)")
+            for metrics in metricsFromMetricsAliasesList{
+                // Any metrics should refer to metrics1
+                XCTAssert(metrics == metrics1, "metrics should be equal to metrics1")
+                XCTAssert(metrics === metrics1, "metrics should be metrics1")
             }
         }catch{
-            XCTFail("\(error)")
+            XCTFail("metricsFromAliasesOfMetrics resolution did fail")
         }
+
     }
 }
