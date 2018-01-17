0cbca372ab2bf70efa88eaea46c91c6b95892800
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-01-17 14:49:20 +0100

    Relative generative script configuration

--- BartlebysCore.flexions/generate.sh
+++ BartlebysCore.flexions/generate.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 
-CONFIGURATION_NAME=$1
 if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <CONFIGURATION_NAME>" >&2
-  exit 1
+CONFIGURATION_NAME="default"
+else
+  CONFIGURATION_NAME=$1
 fi
 echo "Using $CONFIGURATION_NAME"
-php -f generate.php "configuration=configurations/configuration.$CONFIGURATION_NAME.json"
+php -f ./generate.php "configuration=configurations/configuration.$CONFIGURATION_NAME.json"
