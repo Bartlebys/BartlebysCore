f119da59b62c9df265480f2f745a9459a84349d3
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-01-17 17:51:57 +0100

    Typo in Shell script

--- BartlebysCore.flexions/generate.sh
+++ BartlebysCore.flexions/generate.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-
-
 if [ "$#" -ne 1 ]; then
 CONFIGURATION_NAME="default"
 else
-  CONFIGURATION_NAME=$1
+CONFIGURATION_NAME=$1
 fi
 echo "Using $CONFIGURATION_NAME"
 php -f ./generate.php "configuration=configurations/configuration.$CONFIGURATION_NAME.json"
