540cecfaa2134f58e6588a2068752d7bc564bffc
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-01-19 11:15:30 +0100

    Reformatted Package file

--- Package.swift
+++ Package.swift
@@ -1,27 +1,19 @@
 // swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// https://github.com/apple/swift-package-manager/blob/master/Documentation/PackageDescriptionV4.md
 
 import PackageDescription
 
 let package = Package(
     name: "BartlebysCore",
     products: [
-        .library(
-            name: "BartlebysCore",
-            targets: ["BartlebysCore"]
-        ),
-    ],
+        .library(name: "BartlebysCore", targets: ["BartlebysCore"]),
+        ],
     dependencies: [
-       .package(url: "https://github.com/Bartlebys/BTree", from: "4.0.0"),
-    ],
+        .package(url: "https://github.com/Bartlebys/BTree", from: "4.0.0"),
+        ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "BartlebysCore",
-            dependencies: ["BTree"]),
-        .testTarget(
-            name: "BartlebysCoreTests",
-            dependencies: ["BartlebysCore"]),
-    ]
+        .target(name: "BartlebysCore", dependencies: ["BTree"]),
+        .testTarget( name: "BartlebysCoreTests", dependencies: ["BartlebysCore"]),
+        ]
 )
