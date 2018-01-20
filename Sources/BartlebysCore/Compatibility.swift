49031ca304b9e40514426d588c63144d8dbff177
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-01-20 11:56:16 +0100

    Added Compatibility.swift to group the cross platform adapters

---
+++ Sources/BartlebysCore/Compatibility.swift
@@ -0,0 +1,64 @@
+//
+//  Compatibility.swift
+//  BartlebysCore
+//
+//  Created by Benoit Pereira da silva on 20/01/2018.
+//  Copyright © 2018 Bartleby. All rights reserved.
+//
+
+import Foundation
+#if os(Linux)
+    import Glibc
+#endif
+
+// We store in this file the Compatibility Adjustments.
+// We want to provide a consistent stack on any Platform.
+
+#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+
+    public typealias CopyingProtocol = NSCopying
+
+    public func AbsoluteTimeGetCurrent()->Double{
+        return Double(CFAbsoluteTimeGetCurrent())
+    }
+
+    public func random<T: BinaryInteger> (_ n: T) -> T {
+        return numericCast( arc4random_uniform( numericCast(n) ) )
+    }
+
+#elseif os(Linux)
+
+    public protocol CopyingProtocol {}
+
+    open class Object : NSObject {}
+
+    public protocol CopyingProtocol {}
+
+    public func AbsoluteTimeGetCurrent()->Double{
+        return 0 // @todo linux
+    }
+
+    public class Progress {
+        public var totalUnitCount: Int64 = 0
+        public var completedUnitCount: Int64 = 0
+        public init(){}
+    }
+
+    public func NSLocalizedString(_ key: String, tableName: String, comment: String) -> String{
+        return key   // @Todo Linux
+    }
+
+    public func random<T: BinaryInteger> (_ n: T) -> T {
+        precondition(n > 0)
+
+        let upperLimit = RAND_MAX - RAND_MAX % numericCast(n)
+
+        while true {
+            let x = Glibc.random()
+            if x < upperLimit { return numericCast(x) % n }
+        }
+    }
+
+#endif
+
+
--- Sources/BartlebysCore/Object.swift
+++ Sources/BartlebysCore/Object.swift
@@ -9,80 +9,12 @@
 import Foundation
 import Dispatch
 
-/*
- NOTES @todo Bartleby Fusion
- - Server side : Primary is now encoded using "id" not "_id" (this may have a large impact server side.
- - We always use Model:Object as ancestor to implement by hand core behaviour.
- - Consequently we have replaced variable $inheritancePrefix in the generative blocks
- */
-
 #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
 
     open class Object : NSObject{}
 
-    public typealias CopyingProtocol = NSCopying
-
-    public func AbsoluteTimeGetCurrent()->Double{
-        return Double(CFAbsoluteTimeGetCurrent())
-    }
-
-    public func random<T: BinaryInteger> (_ n: T) -> T {
-        return numericCast( arc4random_uniform( numericCast(n) ) )
-    }
-
-
 #elseif os(Linux)
-
-    import Glibc
 
     open class Object : NSObject {}
 
-    public protocol CopyingProtocol {}
-
-    public func AbsoluteTimeGetCurrent()->Double{
-        return 0 // @todo linux
-    }
-
-    public func random<T: BinaryInteger> (_ n: T) -> T {
-        precondition(n > 0)
-
-        let upperLimit = RAND_MAX - RAND_MAX % numericCast(n)
-
-        while true {
-            let x = Glibc.random()
-            if x < upperLimit { return numericCast(x) % n }
-        }
-    }
-
 #endif
-
-public extension Object{
-
-    // MARK: - Thread Safety?
-
-    public static func syncOnMain(execute block: () -> Void) {
-        if Thread.isMainThread {
-            block()
-        } else {
-            DispatchQueue.main.sync(execute: block)
-        }
-    }
-
-    public static func syncThrowableOnMain(execute block: () throws -> Void) rethrows-> (){
-        if Thread.isMainThread {
-            try block()
-        } else {
-            try DispatchQueue.main.sync(execute: block)
-        }
-    }
-
-    public static func syncOnMainAndReturn<T>(execute work: () throws -> T) rethrows -> T {
-        if Thread.isMainThread {
-            return try work()
-        } else {
-            return try DispatchQueue.main.sync(execute: work)
-        }
-    }
-
-}
-
