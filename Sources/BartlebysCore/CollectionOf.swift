18d4a37ad96df8822d6ca18eb2014a1a5c97ee41
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-01-23 10:15:15 +0100

    Suspended set accessor

--- Sources/BartlebysCore/CollectionOf.swift
+++ Sources/BartlebysCore/CollectionOf.swift
@@ -280,17 +280,18 @@ open class CollectionOf<T> : Collection, Sequence,IndistinctCollection, Codable,
       }
    }
 
+   /*
    /// Returns a set with all the collected elements
    ///
    /// - Returns: the extracted set.
-   public func setView<T>() -> Set<T> where T : Hashable{
+   public func setView<T>() -> Set<T>{
       var set = Set<T>()
       for item in self._items{
          set.insert(item)
       }
       return set
    }
-
+*/
    // MARK: - Codable
 
    public enum CollectionCodingKeys: String, CodingKey {
