b58ec7d01726da6edc42e095f232834a5e9eedb8
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-01-25 07:45:53 +0100

    Intregrating PString  and Pluralization.

---
+++ Sources/BartlebysCore/PString.swift
@@ -0,0 +1,239 @@
+//
+//  PString.swift
+//  BartlebyCore
+//
+//  Created by Benoit Pereira da Silva on 19/12/2015.
+//  Copyright © 2015 https://pereira-da-silva.com 
+
+import Foundation
+
+
+// The goal of PString is to facilitate PHP to SWIFT port
+// It focuses on translating litteraly the PHP style string processing.
+// For example Flexions' Pluralization.php has been ported to swift in a few minutes
+// You can compare Pluralization.swift & Pluralization.php
+// We use static func to distinguish from Swift String System
+
+// MARK: - global PHP Style String functions
+
+public struct PString {
+
+    public static func strtoupper(_ string: String) -> String {
+        return string.uppercased()
+    }
+
+    public static func strtolower(_ string: String) -> String {
+        return string.lowercased()
+    }
+
+    /**
+     Left trims the characters specified in the characterSet
+
+     E.g:
+     + PString.ltrim("   *   Hello    *    ",characterSet: NSCharacterSet(charactersInString:" *")) // returns "Hello    *    "
+     + PString.ltrim(",A,B,C",characterSet: NSCharacterSet(charactersInString:",")) // Returns "A,B,C"
+
+     - parameter string:       the string
+     - parameter characterSet: the character set (White spaces and new line by default)
+
+     - returns: the string
+     */
+    public static func ltrim(_ string: String, characterSet: CharacterSet=CharacterSet.whitespacesAndNewlines) -> String {
+        if let range = string.rangeOfCharacter(from: characterSet.inverted) {
+            return String(string[range.lowerBound..<string.endIndex])
+        }
+        return string
+    }
+
+    /**
+     Left trims the characters specified in the characters
+
+     E.g:
+     + PString.ltrim("   *   Hello    *    ",characters:" *") // returns "Hello    *    "
+     + PString.ltrim(",A,B,C",characters:",")) // Returns "A,B,C"
+
+     - parameter string:       the string
+     - parameter characterSet: the character set (White spaces and new line by default)
+
+     - returns: the string
+     */
+    public static func ltrim(_ string: String, characters: String) -> String {
+        return ltrim(string, characterSet: CharacterSet(charactersIn:characters) )
+    }
+
+
+    /**
+     Right trim the characters specified in the characterSet
+
+     - parameter string:       the string
+     - parameter characters: the character set (White spaces and new line by default)
+
+     - returns: the string
+     */
+
+    public static func rtrim(_ string: String, characterSet: CharacterSet=CharacterSet.whitespacesAndNewlines) -> String {
+        if let range = string.rangeOfCharacter(from: characterSet.inverted, options: NSString.CompareOptions.backwards) {
+            return String(string[string.startIndex...range.lowerBound])
+        }
+        return string
+    }
+    /**
+     Right trim the characters specified in the characters
+
+     - parameter string:     the string
+     - parameter characters: the characters
+
+     - returns: the string
+     */
+    public static func rtrim(_ string: String, characters: String) -> String {
+        return rtrim(string, characterSet: CharacterSet(charactersIn:characters) )
+    }
+
+
+    public static func trim(_ string: String,characters: String) -> String {
+        return rtrim(ltrim(string,characters:characters),characters:characters)
+    }
+
+
+
+
+    public static func trim(_ string: String) -> String {
+        return rtrim(ltrim(string))
+    }
+
+
+
+    ///Returns a sub string
+    ///behaves 100% like PHP substring http://php.net/manual/en/function.substr.php
+    ///
+    /// - Parameters:
+    /// - parameter string: the string
+    /// - parameter start:  If start is negative, the returned string will start at the start'th character from the end of string.
+    /// - Returns: the sub string
+    public static func substr(_ string: String, _ start: Int) -> String {
+        return PString.substr(string, start, nil)
+    }
+
+    /**
+     Returns a sub string
+     behaves 100% like PHP substring http://php.net/manual/en/function.substr.php
+
+     - parameter string: the string
+     - parameter start:  If start is negative, the returned string will start at the start'th character from the end of string.
+     - parameter length: length
+
+     - returns: the sub string
+     */
+    public static func substr(_ string: String, _ start: Int, _ length: Int?) -> String {
+
+        let strLength=Int(string.count)
+        var start=start
+        let length:Int=length ?? strLength
+
+        if start<0{
+            start=strLength+start
+        }
+
+        var rightPos:Int=start+length
+
+        if length<0{
+            rightPos=strLength+length
+        }
+
+        var leftPos:Int=start
+
+        leftPos =  max(0,leftPos)
+        rightPos = max(0,rightPos)
+
+        leftPos =  min(strLength,leftPos)
+        rightPos = min(strLength,rightPos)
+
+        if leftPos > rightPos{
+            swap(&leftPos, &rightPos)
+        }
+
+        let startIndex = (leftPos==0) ? string.startIndex : string.index(string.startIndex, offsetBy: leftPos)
+        let endIndex = (rightPos==0) ? string.startIndex : string.index(string.startIndex, offsetBy: rightPos)
+
+        return String(string[startIndex..<endIndex])
+    }
+
+
+    /// Equivalent to substr_replace
+    /// Replaces text within a portion of a string
+    // substr_replace() replaces a string delimited by the start and (optionally) length parameters with the string given in replacement.
+    /// - Parameters:
+    ///   - string: the string
+    ///   - replacement: the remplacement string
+    ///   - start: the start replacement index
+    ///   - length: the optional length of the range to replace
+    /// - Returns: the resulting string
+    public static func substr_replace(_ string:String, replacement:String,start:Int,length:Int?)->String{
+        var result = substr(string, 0, start)
+        result += replacement
+        if let length = length{
+            if length >= 0 {
+                result += substr(string, start+length)
+            }else{
+                let strLen = PString.strlen(string)
+                result += substr(string, strLen, length)
+            }
+        }
+        return result
+    }
+
+
+    public static func substr_replace(_ string:String, replacement:String,start:Int)->String{
+       return substr_replace(string, replacement: replacement, start: start, length: nil)
+    }
+
+
+    public static func strlen(_ string: String) -> Int {
+        return Int(string.count)
+    }
+
+    public static func lcfirst(_ string: String) -> String {
+        var tstring=string
+        let first=tstring.firstCharacterRange()
+        tstring.replaceSubrange(first, with:String(tstring[first]).lowercased())
+        return tstring
+    }
+
+    public static func ucfirst(_ string: String) -> String {
+        var tstring=string
+        let first=tstring.firstCharacterRange()
+        tstring.replaceSubrange(first, with:String(tstring[first]).uppercased())
+        return tstring
+    }
+
+    //preg_match PREG_OFFSET_CAPTURE flag is currently not implemented
+
+    static let PREG_OFFSET_CAPTURE=1
+
+    public static func preg_match(_ pattern: String, _ subject: String, _ matches: inout [String], _ flags: Int = 0, _ offset: Int = 0) -> Int {
+        let subjectWithOffset=substr(subject, offset, strlen(subject))
+        if subjectWithOffset.isMatching(pattern) {
+            return 1
+        }
+        return 0
+    }
+
+    public static func preg_replace (_ pattern: String, _ replacement: String, _ subject: String, _ limit: Int = -1) -> String {
+        do {
+            let regex = try NSRegularExpression(pattern: pattern, options: [.caseInsensitive])
+            return regex.stringByReplacingMatches(in: subject, options: [], range: NSMakeRange(0, strlen(subject)), withTemplate:replacement)
+        } catch {
+            Logger.log("\(error)", category: LogEntry.Category.warning)
+        }
+        return subject
+    }
+
+
+    public static func preg_replace (_ pattern: String, _ replacement: String, _ subject: [String], _ limit: Int = -1) -> [String] {
+        var r=[String]()
+        for subSubject in subject {
+            r.append(preg_replace(pattern, replacement, subSubject, limit))
+        }
+        return r
+    }
+}
---
+++ Sources/BartlebysCore/String+Helpers.swift
@@ -0,0 +1,204 @@
+//
+//  String+Helpers.swift
+//  BartlebysCore
+//
+//  Created by Benoit Pereira da silva on 08/06/2017.
+
+import Foundation
+
+
+// MARK: - String extension
+
+public extension String {
+
+    public func contains(string: String) -> Bool {
+        return (self.range(of: string) != nil)
+    }
+
+    public func contains(_ string: String,compareOptions:NSString.CompareOptions) -> Bool {
+        return (self.range(of: string, options: compareOptions, range: self.fullCharactersRange(), locale: Locale.current) != nil )
+    }
+
+    public func isMatching(_ regex: String) -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: regex, options: [])
+            let matchCount = regex.numberOfMatches(in: self, options: [], range: NSMakeRange(0, self.count))
+            return matchCount > 0
+        } catch {
+            Logger.log("\(error)", category: LogEntry.Category.warning)
+        }
+        return false
+    }
+
+    public func getMatches(_ regex: String, options: NSRegularExpression.Options) -> [NSTextCheckingResult]? {
+        do {
+            let regex = try NSRegularExpression(pattern: regex, options: options)
+            let matches = regex.matches(in: self, options: [], range: NSMakeRange(0, self.count))
+            return matches
+        } catch {
+            Logger.log("\(error)", category: LogEntry.Category.warning)
+        }
+        return nil
+    }
+
+    public func fullCharactersRange() -> Range<Index> {
+        return Range(uncheckedBounds: (lower: self.startIndex, upper: self.endIndex))
+    }
+
+    public func firstCharacterRange()->Range<Index> {
+        return Range(uncheckedBounds: (lower: self.startIndex, upper: self.startIndex))
+    }
+
+    public func lastCharacterRange()->Range<Index> {
+        return Range(uncheckedBounds: (lower: self.endIndex, upper: self.endIndex))
+    }
+
+
+    public func jsonPrettify()->String{
+        do {
+            if let d=self.data(using:.utf8){
+                let jsonObject = try JSONSerialization.jsonObject(with: d, options:[])
+                let jsonObjectData = try JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted)
+                if let prettyString = String(data: jsonObjectData, encoding: .utf8){
+                    return prettyString
+                }
+            }
+        } catch {
+            return self
+        }
+        return self
+    }
+
+    public func fullNSRange()->NSRange{
+        return NSRange(location: 0, length: self.count)
+    }
+
+    /*
+    public func nsRange(from range: Range<String.Index>) -> NSRange {
+        let utf16 = self.utf16
+        let from = range.lowerBound.samePosition(in: utf16)
+        let to = range.upperBound.samePosition(in: utf16)
+        return NSRange(location: utf16.distance(from: utf16.startIndex, to: from),
+                       length: utf16.distance(from: from, to: to))
+    }
+*/
+
+    public func range(from nsRange: NSRange) -> Range<String.Index>? {
+        let utf16 = self.utf16
+        guard
+            let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
+            let to16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location + nsRange.length, limitedBy: utf16.endIndex),
+            let from = from16.samePosition(in: self),
+            let to = to16.samePosition(in: self)
+            else { return nil }
+        return from ..< to
+    }
+
+
+    /// Removes the characters in the sub NSRange.
+    /// The method is ignoring the invalid ranges.
+    ///
+    /// - Parameter range: the range of char to remove
+    public mutating func removeSubNSRange(_ range:NSRange){
+        let rangeEndLocation = range.location + range.length
+        let charCount = self.count
+        let prefixed =  range.location > 0 ? PString.substr(self, 0, range.location) : ""
+        let postFixed = rangeEndLocation < charCount ? PString.substr(self, rangeEndLocation) : ""
+        self = prefixed + postFixed
+    }
+
+
+
+    // MARK: - levenshtein distance
+
+    // https://en.wikipedia.org/wiki/Levenshtein_distance
+
+
+    public func bestCandidate(candidates: [String]) -> (distance:Int,string:String) {
+        var selectedCandidate:String=""
+        var minDistance: Int=Int.max
+        for candidate in candidates {
+            let distance=self.levenshtein(candidate)
+            if distance<minDistance {
+                minDistance=distance
+                selectedCandidate=candidate
+            }
+        }
+        return (minDistance,selectedCandidate)
+    }
+
+
+
+    /// Computes the levenshteinDistance between
+    ///
+    /// - Parameter string: string to compare to
+    /// - Returns: return the distance
+    public func levenshtein( _ string: String) -> Int {
+
+        guard self != "" && string != "" else{
+            return Int.max
+        }
+
+        let a = Array(self.utf16)
+        let b = Array(string.utf16)
+
+        let dist = Array2D(cols: a.count + 1, rows: b.count + 1)
+        for i in 1...a.count {
+            dist[i, 0] = i
+        }
+
+        for j in 1...b.count {
+            dist[0, j] = j
+        }
+
+        for i in 1...a.count {
+            for j in 1...b.count {
+                if a[i-1] == b[j-1] {
+                    dist[i, j] = dist[i-1, j-1]  // noopo
+                } else {
+                    dist[i,j] = min(numbers:
+                        dist[i-1, j] + 1,  // deletion
+                        dist[i, j-1] + 1,  // insertion
+                        dist[i-1, j-1] + 1  // substitution
+                    )
+                }
+            }
+        }
+        return dist[a.count, b.count]
+    }
+
+
+    private func min(numbers: Int...) -> Int {
+        return numbers.reduce(numbers[0], {$0 < $1 ? $0 : $1})
+    }
+
+    private class Array2D {
+        var cols: Int, rows: Int
+        var matrix: [Int]
+
+        init(cols: Int, rows: Int) {
+            self.cols = cols
+            self.rows = rows
+            matrix = Array(repeating:0, count:cols*rows)
+        }
+
+        subscript(col: Int, row: Int) -> Int {
+            get {
+                return matrix[cols * row + col]
+            }
+            set {
+                matrix[cols*row+col] = newValue
+            }
+        }
+
+        func colCount() -> Int {
+            return self.cols
+        }
+
+        func rowCount() -> Int {
+            return self.rows
+        }
+    }
+
+    
+}
