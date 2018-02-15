dabb71f605e6643c0f8b877e0200a834a028d6b7
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-02-15 18:06:00 +0100

    Added HTTPResponse prettyJSON facility

--- Sources/BartlebysCore/HTTPResponse.swift
+++ Sources/BartlebysCore/HTTPResponse.swift
@@ -13,15 +13,33 @@ public class HTTPResponse {
     public var metrics: Metrics?
     public var httpStatus: Status = Status.undefined
     public var content: Data?
-    
-    public var resultString: String? {
-        return self.content != nil ? String(data: self.content!, encoding: .utf8) : ""
+
+
+    /// Encodes the content to an utf8 String
+    public var rawString: String? {
+        return self.content != nil ? String(data: self.content!, encoding: .utf8) : nil
     }
+
+
+    /// Encodes the content to a Pretty JSON
+    public var prettyJSON: String? {
+        guard let data = self.content else{
+            return nil
+        }
+        do{
+            let container = try JSONSerialization.jsonObject(with:data, options: JSONSerialization.ReadingOptions.allowFragments)
+            let reEncodedJSON = try JSONSerialization.data(withJSONObject: container, options: JSONSerialization.WritingOptions.prettyPrinted)
+            return String(data:reEncodedJSON,encoding:.utf8)
+        }catch{
+            return "\(error)"
+        }
+    }
+
 
     public init(metrics:Metrics, httpStatus:Status,content:Data?) {
         self.metrics = metrics
         self.httpStatus = httpStatus
         self.content = content
     }
-    
+
 }
