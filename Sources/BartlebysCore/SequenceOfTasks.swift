b555cb441d8b92f1bee74511a9a0789ac3bbfa3a
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-04-21 14:00:45 +0200

    SequenceOfTasks: sequence.taskCompleted() must be called explicitly on any discreet Task completion

--- Sources/BartlebysCore/SequenceOfTasks.swift
+++ Sources/BartlebysCore/SequenceOfTasks.swift
@@ -45,29 +45,34 @@ public class SequenceOfTasks<T:Any> {
     /// If not you can call
     public var chainTheTasksAutomatically: Bool = true
 
+    public fileprivate(set) var isPaused: Bool = false
+
     public fileprivate(set) var states:[TaskCompletionState] = [TaskCompletionState]()
 
 
     /// The referenced task handling closure used to proceed asynchronously a discreet task.
-    public fileprivate(set) var taskHandler:(_ item: inout T, _ sequence: SequenceOfTasks<T> , _ taskCompletion: @escaping(TaskCompletionState)->()) -> ()
-
+    /// on task completion you must call sequence.taskCompleted()
+    fileprivate  var _taskHandler:(_ item: inout T, _ sequence: inout SequenceOfTasks<T>)->()
 
     /// The reference sequence completion closure
     public fileprivate(set) var onSequenceCompletion:((_ success:Bool)->())
+
+
+    // MARK: - API
 
     /// The constructor.
     ///
     /// - Parameters:
     ///   - items: the items to be sequentially processed
-    ///   - taskHandler: the handler that is called on each item
+    ///   - taskHandler: the discreet task handler (call sequence.taskCompleted() on completion)
     ///   - onSequenceCompletion: : the completion handler called when all the tasks of the sequence has been executed.
     ///   - delayBetweenTasks: the execution delay between the end of the current tasks and its successor.
     public init( items: inout [T],
-                 taskHandler: @escaping(_ item: inout T, _ sequence: SequenceOfTasks<T>, _ taskCompletion: @escaping(TaskCompletionState) ->()) -> (),
+                 taskHandler: @escaping(_ item: inout T, _ sequence: inout SequenceOfTasks<T>)->(),
                  onSequenceCompletion: @escaping(_ success: Bool) -> () ,
                  delayBetweenTasks: TimeInterval = 0) {
         self._items = items
-        self.taskHandler = taskHandler
+        self._taskHandler = taskHandler
         self.onSequenceCompletion = onSequenceCompletion
         self._delay = delayBetweenTasks
     }
@@ -75,15 +80,29 @@ public class SequenceOfTasks<T:Any> {
 
     /// Starts the sequence
     public func start(){
-        if self._items.count > 0{
-            let _ = self._runTask(at: 0)
-        }else{
-            self._endOfTheSequence()
+        if !self.isPaused {
+            if self._items.count > 0{
+                let _ = self._runTask(at: self.index)
+            }else{
+                self._endOfTheSequence()
+            }
         }
     }
 
+    /// Resumes the sequence
+    public func resume(){
+        self.isPaused = false
+        self.start()
+    }
 
-    /// Cancels the sequence will trigger a non successful completion.
+    /// Pauses the sequence
+    public func pause(){
+        self.isPaused = true
+    }
+
+
+    /// Cancels the sequence
+    /// and triggers a non successful completion.
     public func cancel(){
         while self.index < self._items.count{
             self.index += 1
@@ -93,19 +112,34 @@ public class SequenceOfTasks<T:Any> {
     }
 
 
+    /// Should be called on any task completion.
+    ///
+    /// - Parameter state: the completion state.
+    public func taskCompleted(_ state:TaskCompletionState){
+        self._onTaskCompletion(state)
+    }
+
+    /// Runs the next task.
+    /// - Returns: true if there was a task.
+    public func runNextTask() -> Bool{
+        self.index += 1
+        self._runTask(at: self.index)
+        return self.index < self._items .count
+    }
+
+
+    // MARK: - Task running logic
 
     /// Runs the discreet task at a given index and stores its completion state.
     ///
     /// - Parameter index: the index.
     fileprivate func _runTask(at index:Int){
+        var selfReference = self
         if index == self._items.count{
             self._endOfTheSequence()
         }else{
             if index < self._items.count {
-                self.taskHandler(&self._items[index], self, { taskState in
-                    self._onTaskCompletion(taskState)
-                }
-                )
+                self._taskHandler(&self._items[index], &selfReference)
             }else{
                 // can occur on external mutation of the referenced items list.
                 self._onTaskCompletion(.failure(withError: TaskCompletionError.unexistingIndex))
@@ -125,7 +159,7 @@ public class SequenceOfTasks<T:Any> {
                 return
             }
         }
-        if self.chainTheTasksAutomatically{
+        if self.chainTheTasksAutomatically && self.isPaused == false{
             if self._delay == 0 {
                 self.queue.async {
                     let _ = self.runNextTask()
@@ -139,16 +173,6 @@ public class SequenceOfTasks<T:Any> {
             }
         }
     }
-
-
-    /// Runs the next task.
-    /// - Returns: true if there was a task.
-    public func runNextTask() -> Bool{
-        self.index += 1
-        self._runTask(at: self.index)
-        return self.index < self._items .count
-    }
-
 
     /// Called to conclude the sequence.
     fileprivate func _endOfTheSequence(){
