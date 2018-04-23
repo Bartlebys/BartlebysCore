b4c6abb0dfea176d63eb493724fcd7bcee48fdc6
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-04-23 10:18:17 +0200

    Added SequenceOfTasks mutation unit test case

--- Tests/BartlebysCoreTests/SequenceOfTasksTests.swift
+++ Tests/BartlebysCoreTests/SequenceOfTasksTests.swift
@@ -18,7 +18,8 @@ class SequenceOfTasksTests: XCTestCase {
     static var allTests = [
         ("test001_simple_sequence", test001_simple_sequence,
          "test002_simple_sequence_with_a_delay",test002_simple_sequence_with_a_delay,
-         "test003_simple_async_sequence",test003_simple_async_sequence),
+         "test003_simple_async_sequence",test003_simple_async_sequence,
+         "test004_async_sequence_with_dataSource_mutation",test004_async_sequence_with_dataSource_mutation),
         ]
 
     func test001_simple_sequence() {
@@ -86,6 +87,31 @@ class SequenceOfTasksTests: XCTestCase {
         wait(for: [expectation], timeout: 2.0)
     }
 
+    func test004_async_sequence_with_dataSource_mutation() {
+
+        let expectation = XCTestExpectation(description: "Simple sequence validation")
+
+        var items = [ 1, 2, 3]
+        var result = 0
+
+        let tasks = SequenceOfTasks(items: &items, taskHandler: { (item, sequence) in
+            let w = DispatchWorkItem(block: {
+                if items.count == 3{
+                    // let's mutate once the data source
+                    items.append(4)
+                }
+                result += item
+                sequence.taskCompleted(TaskCompletionState.success)
+            })
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.init(uptimeNanoseconds: 1_000_000), execute: w)
+        }, onSequenceCompletion: { (success) in
+            XCTAssert(result == 10 , "Result should be equal to 10 current value: \(result)")
+            expectation.fulfill()
+        },delayBetweenTasks: 0.1)
+
+        tasks.start()
+        wait(for: [expectation], timeout: 2.0)
+    }
 
 
 
