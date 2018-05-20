5fc4e370207d75dde144374174992bb97cbccd49
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2018-05-20 17:59:19 +0200

    Added filesDescription helper to DataPoints

--- Sources/BartlebysCore/DataPoint+Helpers.swift
+++ Sources/BartlebysCore/DataPoint+Helpers.swift
@@ -61,5 +61,52 @@ extension DataPoint{
         return nil
     }
 
-    
+
+
+
+    /// A Debug method that outputs a description of the datapoint files.
+    ///
+    /// - Returns: a lisible description
+    public func filesDescription()->String{
+        var description = "\n## Files Snapshot ##\nDatapoint.identifier: \(self.identifier)"
+        if let fileStorage = self.storage as? FileStorage{
+            let baseUrl = fileStorage.baseUrl
+            description += "\n\(baseUrl.lastPathComponent)/"
+            self._scanFolder(url: baseUrl, level:1, description: &description)
+        }
+        description += "\n"
+        return description
+    }
+
+    fileprivate func _scanFolder(url:URL,level:Int, description: inout String, limit : Int = 50){
+        do{
+            let content = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: [.skipsHiddenFiles])
+            let sortedContent = content.sorted(by: { (u1, u2) -> Bool in
+                return u1.lastPathComponent.compare(u2.lastPathComponent) == ComparisonResult.orderedAscending
+            })
+            let size = try FileManager.default.sizeOfFolder(at: url.path)
+            description += " \(content.count) file\(content.count > 1 ? "s" : "") \(size.stringFileSize)"
+            let indentation = String.init(repeating: "\t", count: level)
+            var counter = 0
+            for file in sortedContent{
+                if counter < limit{
+                    var isDirectory: ObjCBool = false
+                    if FileManager.default.fileExists(atPath: file.path, isDirectory: &isDirectory) {
+                        if isDirectory.boolValue == true{
+                            description += "\n\(indentation)\(file.lastPathComponent)/"
+                            self._scanFolder(url: file,level: level + 1, description: &description)
+                        }else{
+                            description += "\n\(indentation)\(file.lastPathComponent)"
+                        }
+                    }
+                }else if counter == limit{
+                     description += "\n\(indentation)..."
+                }
+                counter += 1
+            }
+        }catch{
+            description += "\n\(error)"
+        }
+    }
+
 }
