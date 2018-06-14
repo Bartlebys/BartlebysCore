47675aada72a5f97689dc5bd0fa6b7081d8fcc49
Laurent <laurent@staxriver.com>
2018-06-14 15:26:02 +0200

    Paths refactoring

--- Sources/BartlebysCore/Paths.swift
+++ Sources/BartlebysCore/Paths.swift
@@ -12,30 +12,24 @@ public class Paths {
     
     // MARK: - IO
     
-    /// Default application directory name, should be defined for each application
-    /// On macOS, we write in Application Support/<applicationDirectoryName>/<relativeFolderPath>/file
-    public static var applicationDirectoryName: String =  Default.NO_NAME
-
-
     enum PathsError : Error {
         case notFound
     }
     
-    /// The default baseDirectoryURL
-    /// On macOS, we write in /Application Support/<applicationDirectoryName>
+    /// The document directory URL
+    /// On macOS, we write in /Documents/
     /// - Returns: the base directory URL
-    public static var baseDirectoryURL:URL {
+    public static var documentsDirectoryURL:URL {
         #if os(iOS) || os(macOS) || os(tvOS) || os(watchOS)
-            let urls = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)
-            if let url = urls.first {
-                return url.appendingPathComponent(Paths.applicationDirectoryName, isDirectory: true)
-            }
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        if let url = urls.first {
+            return url
+        }
         #elseif os(Linux)
-            // linux @todo
+        // linux @todo
         #endif
         return URL(fileURLWithPath: "Invalid")
     }
-
 
     /// Returns the URL of the valid default directory
     /// On macOS, we write in Application Support/(applicationDirectoryName)/(relativeFolderPath)/
@@ -47,7 +41,7 @@ public class Paths {
         #if os(iOS) || os(macOS) || os(tvOS) || os(watchOS)
             let urls = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)
             if let _url = urls.first {
-                let applicationDirectoryURL = _url.appendingPathComponent(Paths.applicationDirectoryName, isDirectory: true)
+                let applicationDirectoryURL = _url.appendingPathComponent(Paths.legacyApplicationDirectoryName, isDirectory: true)
                 if let relativeFolderPath = relativeFolderPath {
                     return applicationDirectoryURL.appendingPathComponent(relativeFolderPath, isDirectory: true)
                 } else {
@@ -60,5 +54,47 @@ public class Paths {
         throw PathsError.notFound
     }
 
+    // MARK: - Legacy
+    
+    /// Default application directory name, should be defined for each application
+    /// On macOS, we write in Application Support/<applicationDirectoryName>/<relativeFolderPath>/file
+    public static var legacyApplicationDirectoryName: String =  Default.NO_NAME
+    
+    /// The default baseDirectoryURL
+    /// On macOS, we write in /Application Support/<applicationDirectoryName>
+    /// - Returns: the base directory URL
+    public static var legacyBaseDirectoryURL:URL {
+        #if os(iOS) || os(macOS) || os(tvOS) || os(watchOS)
+        let urls = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)
+        if let url = urls.first {
+            return url.appendingPathComponent(Paths.legacyApplicationDirectoryName, isDirectory: true)
+        }
+        #elseif os(Linux)
+        // linux @todo
+        #endif
+        return URL(fileURLWithPath: "Invalid")
+    }
+    
+    public static func moveLegacyFiles() {
+        
+        print("Paths.legacyBaseDirectoryURL = \(Paths.legacyBaseDirectoryURL)")
+        print("Paths.documentsDirectoryURL = \(Paths.documentsDirectoryURL)")
 
+        do {
+            var isDirectory: ObjCBool = true
+            if FileManager.default.fileExists(atPath: Paths.legacyBaseDirectoryURL.path, isDirectory: &isDirectory) {
+                let content: [URL] = try FileManager.default.contentsOfDirectory(at: Paths.legacyBaseDirectoryURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+                for url in content {
+                    do {
+                        try FileManager.default.moveItem(at: url, to: Paths.documentsDirectoryURL.appendingPathComponent(url.lastPathComponent))
+                    } catch {
+                        Logger.log(error)
+                    }
+                }
+            }
+        } catch {
+            Logger.log(error)
+        }
+    }
+    
 }
